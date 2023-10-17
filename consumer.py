f8ee5d027bc574c46fa5034883f815ae57e52853
William Aguilar <wil-20-01@live.com>
2023-10-16 18:04:05 -0600

    including new arch and frontend also multi threading

--- consumer.py
+++ consumer.py
@@ -1,42 +1,25 @@
-import io
-from flask import Flask, Response
 from kafka import KafkaConsumer
-from PIL import Image
+import cv2
+import numpy as np
+from flask import Flask, Response, render_template
 
-class ImageProcessor:
-    def __init__(self, width=1000, height=800):
-        self.WIDTH = width
-        self.HEIGHT = height
-
-    def resize_image(self, image_bytes):
-        image = Image.open(io.BytesIO(image_bytes))
-        image = image.resize((self.WIDTH, self.HEIGHT), Image.ANTIALIAS)
-        resized_image_bytes = io.BytesIO()
-        image.save(resized_image_bytes, format="JPEG")
-        return resized_image_bytes.getvalue()
-
-class KafkaImageStream:
-    def __init__(self, topic, bootstrap_servers):
-        self.consumer = KafkaConsumer(topic, bootstrap_servers=bootstrap_servers)
-        self.image_processor = ImageProcessor()
-
-    def get_frame(self):
-        for message in self.consumer:
-            resized_image = self.image_processor.resize_image(message.value)
-            yield (
-                b'--frame\r\n'
-                b'Content-Type: image/jpeg\r\n\r\n' + resized_image + b'\r\n\r\n'
-            )
+# Configuración del consumidor Kafka
+consumer = KafkaConsumer('video-frames', bootstrap_servers='localhost:9092')
 
 app = Flask(__name__)
 
+def generate_frames():
+    for message in consumer:
+        yield (b'--frame\r\n'
+               b'Content-Type: image/jpeg\r\n\r\n' + message.value + b'\r\n')
+
 @app.route('/')
-def index():
-    kafka_stream = KafkaImageStream('my-topic', 'localhost:9092')
-    return Response(
-        kafka_stream.get_frame(),
-        mimetype='multipart/x-mixed-replace; boundary=frame'
-    )
+def video_feed():
+    return render_template('index.html')
+
+@app.route('/video')
+def video():
+    return Response(generate_frames(), mimetype='multipart/x-mixed-replace; boundary=frame')
 
 if __name__ == '__main__':
-    app.run()
+    app.run(host='0.0.0.0', port=5000)
--- producer.py
+++ producer.py
@@ -1,10 +1,11 @@
 import cv2
-import numpy as np
 from kafka import KafkaProducer
-from kafka.errors import KafkaError
+import threading
+import numpy as np
+from queue import Queue
 
 class ObjectDetector:
-    def __init__(self, model_path, prototxt_path, confidence_threshold=0.80):
+    def __init__(self, model_path, prototxt_path, confidence_threshold=0.50):
         self.neural_network = cv2.dnn.readNetFromCaffe(prototxt_path, model_path)
         self.confidence_threshold = confidence_threshold
         self.classes = ["fondo", "avión", "bicicleta", "pájaro", "barco", "botella",
@@ -16,12 +17,11 @@ class ObjectDetector:
         self.topic = 'my-topic'
 
     def detect_objects(self, frame):
+        frame = cv2.flip(frame, 1)
         (h, w) = frame.shape[:2]
         frame_blob = cv2.dnn.blobFromImage(cv2.resize(frame, (300, 300)), 0.007843, (300, 300), 127.5)
         self.neural_network.setInput(frame_blob)
         neural_network_output = self.neural_network.forward()
-
-        detected_objects = []
 
         for i in np.arange(0, neural_network_output.shape[2]):
             confidence = neural_network_output[0, 0, i, 2]
@@ -40,38 +40,82 @@ class ObjectDetector:
                 endY = int(endY - box_height * (1 - shrink_factor) / 2)
 
                 if startY < endY and startX < endX:
-                    rounded_percentage = round(confidence * 100 / 10) * 10
-                    label = "{}: {}%".format(self.classes[idx], rounded_percentage)
-                    cv2.rectangle(frame, (startX, startY), (endX, endY), self.bbox_colors[idx], 2)
-                    cv2.putText(frame, label, (startX, startY - 5), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 0), 2)
-                    detected_objects.append((label, frame.copy()))
+                    if self.classes[idx] == "botella":
+                        # Si se detecta una botella, aplica desenfoque a la región de la botella
+                        bottle_region = frame[startY:endY, startX:endX]
+                        cv2.rectangle(frame, (startX, startY), (endX, endY), self.bbox_colors[idx], 2)
+                        label = "{}: {}%".format(self.classes[idx], round(confidence * 100, 2))
+                        cv2.putText(frame, label, (startX, startY - 5), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 0), 2)
 
-        return detected_objects
+                        blurred_bottle_region = cv2.GaussianBlur(bottle_region, (95, 95), 0)
+                        frame[startY:endY, startX:endX] = blurred_bottle_region
+                    else:
+                        cv2.rectangle(frame, (startX, startY), (endX, endY), self.bbox_colors[idx], 2)
+                        label = "{}: {}%".format(self.classes[idx], round(confidence * 100, 2))
+                        cv2.putText(frame, label, (startX, startY - 5), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 0), 2)
+
+        return frame  # Devuelve el frame con objetos detectados
 
     def send_to_kafka(self, detected_objects):
         for label, detected_frame in detected_objects:
             _, data = cv2.imencode('.jpeg', detected_frame)
             future = self.producer.send(self.topic, data.tobytes())
-            
-            try:
-                future.get(timeout=10)
-            except KafkaError as e:
-                print(e)
 
-def main():
-    detector = ObjectDetector('MobileNetSSD_deploy.caffemodel', 'MobileNetSSD_deploy.prototxt.txt')
 
-    cap = cv2.VideoCapture(0)
+class FrameProducer:
+    def __init__(self, kafka_server, topic, model_path, prototxt_path):
+        self.producer = KafkaProducer(bootstrap_servers=kafka_server)
+        self.video_capture = cv2.VideoCapture(1)
+        self.topic = topic
+        self.capture_thread = None
+        self.send_thread = None
+        self.detector = ObjectDetector(model_path, prototxt_path)
+        self.frame_queue = Queue()
 
-    while True:
-        success, frame = cap.read()
+    def capture_frames(self):
+        while True:
+            ret, frame = self.video_capture.read()
+            if not ret:
+                break
 
-        if not success:
-            break
+            # Detección de objetos
+            detected_frame = self.detector.detect_objects(frame)
 
-        frame = cv2.flip(frame, 1)
-        detected_objects = detector.detect_objects(frame)
-        detector.send_to_kafka(detected_objects)
+            # Agrega el frame detectado a la cola
+            self.frame_queue.put(detected_frame)
+
+    def start(self):
+        self.capture_thread = threading.Thread(target=self.capture_frames)
+        self.send_thread = threading.Thread(target=self.send_frames)
+        self.capture_thread.start()
+        self.send_thread.start()
+
+    def send_frames(self):
+        while True:
+            # Toma un frame de la cola
+            detected_frame = self.frame_queue.get()
+            _, buffer = cv2.imencode('.jpg', detected_frame, [int(cv2.IMWRITE_JPEG_QUALITY), 80])
+
+            # Envío del frame con objetos detectados
+            self.producer.send(self.topic, value=buffer.tobytes())
+
+    def stop(self):
+        if self.capture_thread:
+            self.capture_thread.join()
+        if self.send_thread:
+            self.send_thread.join()
 
 if __name__ == "__main__":
-    main()
+    kafka_server = 'localhost:9092'
+    topic = 'video-frames'
+    model_path = 'MobileNetSSD_deploy.caffemodel'
+    prototxt_path = 'MobileNetSSD_deploy.prototxt.txt'
+
+    frame_producer = FrameProducer(kafka_server, topic, model_path, prototxt_path)
+    frame_producer.start()
+
+    try:
+        frame_producer.capture_thread.join()
+    except KeyboardInterrupt:
+        print("Deteniendo el productor...")
+        frame_producer.stop()
